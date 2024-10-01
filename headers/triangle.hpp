5cc7b79e07bbd3c7802cdb373c50547d0c4c6751
ElkCl0ner <lilinwei124@yahoo.com>
2024-10-01 14:00:14 -0400

    modified triangle struct

--- headers/triangle.hpp
+++ headers/triangle.hpp
@@ -5,8 +5,10 @@
 namespace bvh {
 
   typedef struct {
-    vec3<float> a, b, c;   // Vertices
-    vec3<float> normal;    // Normal vector
+    vec3<float> vertices[3];    // Vertices coordinates
+    vec3<float> uv[3];          // Texture coordinates
+    vec3<float> normals[3];     // Normal vectors
+    bool smooth;            // Smooth shading
   } Triangle;
 
 }
