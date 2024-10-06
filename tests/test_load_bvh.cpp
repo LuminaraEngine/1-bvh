dc248a29df8e141f7b1ca81d1012b202f00303dd
ElkCl0ner <lilinwei124@yahoo.com>
2024-10-05 23:01:21 -0400

    added custom assert

--- tests/test_load_bvh.cpp
+++ tests/test_load_bvh.cpp
@@ -1,4 +1,5 @@
 #include <test_load_bvh.hpp>
+#include <custom_assert.hpp>
 #include <object.hpp>
 
 bool bvh::test_load_bvh() {
@@ -7,6 +8,8 @@ bool bvh::test_load_bvh() {
   if (obj == nullptr) {
     return false;
   }
+
+  custom_assert(obj->num_triangles == 12, "num_triangles should be 12");
 
   // TODO: check that the object was loaded correctly
 
