7a79c87ce7c0bd91d7e20e01c1116ec47fecdb2f
Ext1nct9 <william.zhang3@mail.mcgill.ca>
2024-10-15 14:52:51 -0400

    Build_bvh tests and function

--- headers/bvh.hpp
+++ headers/bvh.hpp
@@ -8,5 +8,6 @@ namespace bvh {
 
   BvhNode *precompute_bvh(Triangle* tris, int start, int end);
   BvhNode *build_bvh_from_objects(Object *objs, int num_objs, int start);
+  BoundingBox computeCombinedBoundingBox(const std::vector<BvhNode*>& bvhNodes);
 
 }
